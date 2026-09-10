fb1563cdb151604a76ccefaf5af39f077fb0c387
board-mate <board_mate@naver.com>
2026-09-10 11:37:16 +0900

    Delete SUPABASE_VERIFY_TARGETED_V29.sql

--- SUPABASE_VERIFY_TARGETED_V29.sql
+++
@@ -1,6 +0,0 @@
--- Verify v11.4.29 targeted repair
-select to_regprocedure('public.boardmate_get_cancel_status(text,uuid)') as cancel_get_rpc,
-       to_regprocedure('public.boardmate_set_cancel_vote(text,uuid,boolean)') as cancel_set_rpc,
-       to_regprocedure('public.boardmate_plakoro_get_setup_pair_v2(text,uuid)') as plakoro_pair_rpc;
-select to_regclass('public.boardmate_game_cancel_votes') as cancel_votes_table;
-select conname, pg_get_constraintdef(oid) as definition from pg_constraint where conname in ('boardmate_rooms_game_check','boardmate_ratings_game_check') order by conname;
