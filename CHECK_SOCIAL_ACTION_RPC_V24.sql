8c8c568778d7a6d456c9a07b0ef6b076f3273b49
board-mate <board_mate@naver.com>
2026-09-10 11:17:38 +0900

    Delete CHECK_SOCIAL_ACTION_RPC_V24.sql

--- CHECK_SOCIAL_ACTION_RPC_V24.sql
+++
@@ -1,4 +0,0 @@
--- Correct signature: boardmate_social_action(text, uuid, jsonb)
-select
-  to_regprocedure('public.boardmate_social_action(text,uuid,jsonb)') is not null
-  as "social action RPC exists";
